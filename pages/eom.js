373e8913f2ae9a57811242fb85fb44f519d694be
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-09-01 20:07:08 +0200

    add styles to page

--- pages/eom.js
+++ pages/eom.js
@@ -1,12 +1,21 @@
 import React from 'react'
-
+import styles from '../styles/eom.module.css'
 function eom({ employee }) {
 
     console.log(employee, "employee")
     return (
         <div className='page-container'>
-            <div>
+            <div className={styles.main}>
                 <h1>Employee of the Month</h1>
+
+                <div className={styles.employeeOfTheMonth} >
+                    <h3>
+                        {employee.name}
+                    </h3>
+                    <h6>{employee.position}</h6>
+                    <img src={employee.image} alt={employee.name} />
+                    <p>{employee.description}</p>
+                </div>
             </div>
         </div>
     )
